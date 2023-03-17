d1bc2124c535b1e8b96684ac1f0ad0442fffcbaf
Gentlemannerss <damonvanbergen@gmail.com>
2023-03-17 09:02:00 +0100

    runAgain added

--- src/Main.java
+++ src/Main.java
@@ -14,44 +14,55 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         Random rand = new Random();
 
-        String[] choices = {"steen", "papier", "schaar"};
-        System.out.println("Voer hier je keuze in, steen, papier of schaar.");
-        String userInput;
-        while (true) {
-            System.out.println("Please enter your choice: ");
-            userInput = scanner.nextLine().toLowerCase();
+        boolean runAgain = true;
+        while (runAgain) {
+            String[] choices = {"steen", "papier", "schaar"};
+            System.out.println("Voer hier je keuze in, steen, papier of schaar.");
+            String userInput;
+            while (true) {
+                System.out.println("Please enter your choice: ");
+                userInput = scanner.nextLine().toLowerCase();
 
-            boolean validInput = false;
-            for (String choice : choices) {
-                if (userInput.equals(choice)) {
-                    validInput = true;
+                boolean validInput = false;
+                for (String choice : choices) {
+                    if (userInput.equals(choice)) {
+                        validInput = true;
+                        break;
+                    }
+                }
+                if (validInput) {
                     break;
+                } else {
+                    System.out.println("Invalid input. Please try again.");
                 }
             }
-            if (validInput) {
-                break;
-            } else {
-                System.out.println("Invalid input. Please try again.");
-            }
-        }
-        int random = rand.nextInt(2);
-        String computerInput = choices[random];
-        System.out.println("The computer choice is: " + computerInput);
+            int random = rand.nextInt(2);
+            String computerInput = choices[random];
+            System.out.println("The computer choice is: " + computerInput);
 
-        if (computerInput.equals("schaar") && userInput.equals("steen")) {
-            System.out.println("Jij hebt gewonnen!");
-        } else if (computerInput.equals("papier") && userInput.equals("schaar")) {
-            System.out.println("Jij hebt gewonnen!");
-        } else if (computerInput.equals("steen") && userInput.equals("papier")) {
-            System.out.println("Jij hebt gewonnen!");
-        } else if (computerInput.equals("steen") && userInput.equals("schaar")) {
-            System.out.println("Je hebt verloren.");
-        } else if (computerInput.equals("papier") && userInput.equals("steen")) {
-            System.out.println("Je hebt verloren.");
-        } else if (computerInput.equals("schaar") && userInput.equals("papier")) {
-            System.out.println("Je hebt verloren.");
-        } else {
-            System.out.println("It's a draw.");
+            if (computerInput.equals("schaar") && userInput.equals("steen")) {
+                System.out.println("Jij hebt gewonnen!");
+            } else if (computerInput.equals("papier") && userInput.equals("schaar")) {
+                System.out.println("Jij hebt gewonnen!");
+            } else if (computerInput.equals("steen") && userInput.equals("papier")) {
+                System.out.println("Jij hebt gewonnen!");
+            } else if (computerInput.equals("steen") && userInput.equals("schaar")) {
+                System.out.println("Je hebt verloren.");
+            } else if (computerInput.equals("papier") && userInput.equals("steen")) {
+                System.out.println("Je hebt verloren.");
+            } else if (computerInput.equals("schaar") && userInput.equals("papier")) {
+                System.out.println("Je hebt verloren.");
+            } else {
+                System.out.println("It's a draw.");
+            }
+            runAgain = systemRunAgain(scanner);
         }
+    }
+
+    private static boolean systemRunAgain(Scanner scannerRunAgain) {
+        System.out.println("Would you like to play another round? (yes/no): ");
+        String anotherConversion = scannerRunAgain.next();
+        scannerRunAgain.nextLine();
+        return anotherConversion.equalsIgnoreCase("yes");
     }
 }
